3667ff22e7d9dfb8aed23ea5d46e68a0372a7c0e
Jaakko Pöyhönen <jaakko.poyhonen@gmail.com>
2024-11-26 04:35:55 +0200

    debug swing

--- WebglLowestQuality/Build/WebglLowestQuality.loader.js
+++ WebglLowestQuality/Build/WebglLowestQuality.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 54600765,
+    wasmFileSize: 54600794,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
