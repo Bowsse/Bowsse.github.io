13095ef9e50c82a7841a91fd56e671ae8f93612f
Jaakko Pöyhönen <jaakko.poyhonen@gmail.com>
2024-11-26 03:59:03 +0200

    broke add force

--- WebglLowestQuality/Build/WebglLowestQuality.loader.js
+++ WebglLowestQuality/Build/WebglLowestQuality.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 54600719,
+    wasmFileSize: 54600832,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
