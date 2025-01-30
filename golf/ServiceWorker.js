70a2ea7b260427b27b4a56745d39626d24f2e573
Jaakko Pöyhönen <jaakko.poyhonen@gmail.com>
2025-01-30 12:33:15 +0200

    golf testbuild

---
+++ golf/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-GolfWithMyself-0.1.0";
+const contentToCache = [
+    "Build/30.1.WebGL.loader.js",
+    "Build/30.1.WebGL.framework.js",
+    "Build/30.1.WebGL.data",
+    "Build/30.1.WebGL.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
